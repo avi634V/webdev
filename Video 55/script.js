dbb0f6011b38d04d91fab223f4bcca8e0fc87c75
avi634V <avi634V@gmail.com>
2025-04-01 17:39:07 +0530

    udpate

---
+++ Video 55/script.js
@@ -0,0 +1,60 @@
+console.log("Hey this is video 55 for js")
+
+
+
+// let a =5;
+// // let a ="Avijit";// var can be changed 
+// letb =6;
+
+// console.log(a+b)
+// // var c="Harry";
+// // const a1 =6; 
+// // a1=a1+6;
+// //  Not allowed as a1 is const
+
+//  console.log(typeof a, typeof b, typeof c)
+
+
+// let x =1;
+// let y=2;
+// console.log(x);
+
+// {   
+//     let x =3;
+//     console.log(x);
+// }
+// console.log(x);
+
+
+// var x=1;
+// var y=2;
+// console.log(x)
+
+// {
+//     var x =66;
+//     console.log(x)
+// }
+// console.log(x)
+
+
+// let x = "Avijit";
+// let y = 22;
+// let z = 3.55;
+// const p = true;
+// let q = undefined;
+// let r = null;
+
+// console.log(x, y, z, p, q, r)
+// console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+
+let o = {
+    "name": "Avijit",
+    "Job role ": "SW Eng",
+    "is_handsome":true 
+}
+console.log(o)
+
+o.salary ="100Crores"
+console.log(o)

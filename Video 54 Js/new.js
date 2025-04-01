85cac2b975ca5b4904b05cfd5c364d0f568b98b4
avi634V <avi634V@gmail.com>
2025-04-01 17:40:08 +0530

    update

---
+++ Video 54 Js/new.js
@@ -0,0 +1,5 @@
+console.log("Hello world")
+console.log("Hello world2")
+console.log("Hello world3")
+console.log("Hello world4")
+
---
+++ Video 54 Js/script.js
@@ -0,0 +1,17 @@
+alert("Hello World");
+console.log("Code is running/.....");
+console.log("Code is aslo running/.....");
+console.log("Code is like a wowo running/.....");
+
+var a=prompt("enter your number");
+var isTrue=confirm(" are u sure ypu want to elave this page and burn this computer")
+if (isTrue){
+    console.log("comouter is blasting")
+}
+else{
+    console.log("computer is not blasting")
+}
+console.log("your number is "  +a);
+
+document.title="Hye i am good"
+document.body.style.backgroundColor= "red"

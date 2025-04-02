9ebad02cd76393d77e8ea57599c1bbdd658e6d59
avi634V <avi634V@gmail.com>
2025-04-02 19:30:44 +0530

    update

---
+++ Video 56/index.js
@@ -0,0 +1,46 @@
+// console.log("hello i am Avijit")
+
+
+// let age = 1;
+// // let grace = 2;
+// // age += grace;// += increment operator age =age+grace
+// // console.log(age);
+// // console.log(age - grace);
+// // console.log(age * grace);
+// // console.log(age / grace);
+// // console.log(age ** grace);
+// // console.log(age % grace);
+
+
+
+// if (age == 18) {
+//     console.log("you can drive")
+// }
+// else if
+//     (age == 0) {
+//     console.log("are u kidding");
+// }
+// else if
+//     (age == 1) {
+//     console.log("are u again kidding");
+// }
+
+// else {
+//     console.log("you cant drive")
+// }
+
+let a = 3;
+let b = 4;
+
+let c = a > b ? (a - b) : (b - a)
+console.log(c);
+
+/* above statement  transalates to
+if (a>b){
+let c=a-b;
+
+}
+else{
+let c=b-a;
+}
+*/ 
---
+++ Video 57/index.js
@@ -0,0 +1,37 @@
+console.log("loop tutorial");
+
+// let a = 1;
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+
+// let obj ={
+//     name:"AVIJIT",
+//     role:"SW",
+//     company:"Pscope"
+// }
+
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key,element)
+
+//     }
+// for (const element of "Avijit") {
+//     console.log(element)
+// }
+
+// let i = 2;
+
+// while (i < 6 ) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+
+// } while (i<6); 

d2bdbaca101ec118c04ed873b00ee8494854ac20
avi634V <avi634V@gmail.com>
2025-04-03 12:04:53 +0530

    update

---
+++ Video 58/index.js
@@ -0,0 +1,39 @@
+
+// function nice(name) {
+//     console.log("Hey " + name +" you are nice")
+//     console.log("Hey " + name +" you are good")
+//     console.log("Hey " + name +" you are very good")
+//     console.log("Hey " + name +" you are ver nice")
+// }
+// nice("Avijit");
+// nice("Rohan");
+// nice("Ram")
+
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+// sum(1, 34);
+
+
+
+// function sum(a, b,c=3) {
+//     return (a + b+c);
+// }
+// result1 = sum(2, 4);
+// result2 = sum(21, 41);
+// result3 = sum(22 ,42);
+
+// console.log("The sum of these number is: ", result1);
+// console.log("The sum of these number is: ", result2);
+// console.log("The sum of these number is: ", result3);
+
+
+const funct1 = (x) => {
+    console.log("i am an arrow fucntion", x);
+}
+
+funct1(32);
+funct1(45);
+funct1(35);
+
---
+++ Video 58/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+function sum(a+b) {
+    return (a + b);
+}
+
+result = sum(2 , 4);
+console.log("The sum of these number is: ", result)
